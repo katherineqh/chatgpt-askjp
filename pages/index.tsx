20f33dc3117913b38bca3f7f30a5f2962c40c933
katherineqh <57205225+katherineqh@users.noreply.github.com>
2023-05-26 13:03:19 +1000

    change jpimage name again

--- pages/index.tsx
+++ pages/index.tsx
@@ -138,7 +138,7 @@ export default function Home() {
                     icon = (
                       <Image
                         key={index}
-                        src="/jpimage1.png"
+                        src="/jpimage2.png"
                         alt="AI"
                         width="40"
                         height="40"
